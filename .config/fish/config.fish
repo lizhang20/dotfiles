be0437c96858000d3b9d77011e69c80c428b0933
piaoliangkb <piaoliangkb@gmail.com>
2020-04-04 21:40:11 +0800

    Another fish prompt format

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,3 +21,25 @@ function fish_greeting
 	echo -e (uname -n | awk '{print " \\\\e[1mHostname: \\\\e[0;32m"$0"\\\\e[0m"}')
         echo		
 end
+
+# Below lines will change fish prompt format like 
+# [date] LaptopName: Username | currentDir (git info) >>> ...
+# Current configuration in functions/fish_prompt.fish
+#
+# function fish_prompt
+#     set_color brblack
+#     echo -n "["(date "+%H:%M")"] "
+#     set_color blue
+#     echo -n (hostname)
+#     if [ $PWD != $HOME ]
+#         set_color brblack
+#         echo -n ':'
+#         set_color yellow
+#         echo -n (basename $PWD)
+#     end
+#     set_color green
+#     printf '%s ' (__fish_git_prompt)
+#     set_color red
+#     echo -n '| '
+#     set_color normal
+# end
